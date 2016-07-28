fa866f1dee0a25822792445811ae71e6096e3a0b
Martijn van Maurik <martijn@vmaurik.nl>
2016-07-28 15:47:45 +0200

    Fix

--- init.zsh
+++ init.zsh
@@ -1,78 +1,50 @@
 #
-# INSTRUCTIONS
+# Provides for an easier use of SSH by setting up ssh-agent.
 #
-#   To enable agent forwarding support add the following to
-#   your .zshrc file:
-#
-#     zstyle :omz:plugins:ssh-agent agent-forwarding on
-#
-#   To load multiple identities use the identities style, For
-#   example:
-#
-#     zstyle :omz:plugins:ssh-agent identities id_rsa id_rsa2 id_github
-#
-#   To set the maximum lifetime of the identities, use the
-#   lifetime style. The lifetime may be specified in seconds
-#   or as described in sshd_config(5) (see TIME FORMATS)
-#   If left unspecified, the default lifetime is forever.
-#
-#     zstyle :omz:plugins:ssh-agent lifetime 4h
-#
-# CREDITS
-#
-#   Based on code from Joseph M. Reagle
-#   http://www.cygwin.com/ml/cygwin/2001-06/msg00537.html
-#
-#   Agent forwarding support based on ideas from
-#   Florent Thoumie and Jonas Pfenniger
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
 #
 
-local _plugin__ssh_env
-local _plugin__forwarding
-
-function _plugin__start_agent()
-{
-  local -a identities
-  local lifetime
-  zstyle -s :omz:plugins:ssh-agent lifetime lifetime
-
-  # start ssh-agent and setup environment
-  /usr/bin/env ssh-agent ${lifetime:+-t} ${lifetime} | sed 's/^echo/#echo/' > ${_plugin__ssh_env}
-  chmod 600 ${_plugin__ssh_env}
-  . ${_plugin__ssh_env} > /dev/null
-
-  # load identies
-  zstyle -a :omz:plugins:ssh-agent identities identities
-  echo starting ssh-agent...
-
-  /usr/bin/ssh-add $HOME/.ssh/${^identities}
-}
-
-# Get the filename to store/lookup the environment from
-if (( $+commands[scutil] )); then
-  # It's OS X!
-  _plugin__ssh_env="$HOME/.ssh/environment-$(scutil --get ComputerName)"
-else
-  _plugin__ssh_env="$HOME/.ssh/environment-$HOST"
+# Return if requirements are not found.
+if [[ "$OSTYPE" == darwin* ]] || (( ! $+commands[ssh-agent] )); then
+  return 1
 fi
 
-# test if agent-forwarding is enabled
-zstyle -b :omz:plugins:ssh-agent agent-forwarding _plugin__forwarding
-if [[ ${_plugin__forwarding} == "yes" && -n "$SSH_AUTH_SOCK" ]]; then
-  # Add a nifty symlink for screen/tmux if agent forwarding
-  [[ -L $SSH_AUTH_SOCK ]] || ln -sf "$SSH_AUTH_SOCK" /tmp/ssh-agent-$USER-screen
+# Set the path to the SSH directory.
+_ssh_dir="$HOME/.ssh"
 
-elif [ -f "${_plugin__ssh_env}" ]; then
-  # Source SSH settings, if applicable
-  . ${_plugin__ssh_env} > /dev/null
-  ps x | grep ${SSH_AGENT_PID} | grep ssh-agent > /dev/null || {
-    _plugin__start_agent;
-  }
-else
-  _plugin__start_agent;
+# Set the path to the environment file if not set by another module.
+_ssh_agent_env="${_ssh_agent_env:-${TMPDIR:-/tmp}/ssh-agent.env}"
+
+# Set the path to the persistent authentication socket.
+_ssh_agent_sock="${TMPDIR:-/tmp}/ssh-agent.sock"
+
+# Start ssh-agent if not started.
+if [[ ! -S "$SSH_AUTH_SOCK" ]]; then
+  # Export environment variables.
+  source "$_ssh_agent_env" 2> /dev/null
+
+  # Start ssh-agent if not started.
+  if ! ps -U "$LOGNAME" -o pid,ucomm | grep -q -- "${SSH_AGENT_PID:--1} ssh-agent"; then
+    eval "$(ssh-agent | sed '/^echo /d' | tee "$_ssh_agent_env")"
+  fi
 fi
 
-# tidy up after ourselves
-unfunction _plugin__start_agent
-unset _plugin__forwarding
-unset _plugin__ssh_env
+# Create a persistent SSH authentication socket.
+if [[ -S "$SSH_AUTH_SOCK" && "$SSH_AUTH_SOCK" != "$_ssh_agent_sock" ]]; then
+  ln -sf "$SSH_AUTH_SOCK" "$_ssh_agent_sock"
+  export SSH_AUTH_SOCK="$_ssh_agent_sock"
+fi
+
+# Load identities.
+if ssh-add -l 2>&1 | grep -q 'The agent has no identities'; then
+  zstyle -a ':prezto:module:ssh:load' identities '_ssh_identities'
+  if (( ${#_ssh_identities} > 0 )); then
+    ssh-add "$_ssh_dir/${^_ssh_identities[@]}" 2> /dev/null
+  else
+    ssh-add 2> /dev/null
+  fi
+fi
+
+# Clean up.
+unset _ssh_{dir,identities} _ssh_agent_{env,sock}
